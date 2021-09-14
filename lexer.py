0ca87f7f853d3993fc3684f6f0ddab90596b0b1e
Chaney Chantipaporn <66577982+ChaneyChanti@users.noreply.github.com>
2021-09-14 01:19:31 -0700

    Add files via upload

--- lexer.py
+++ lexer.py
@@ -1,33 +1,19 @@
-import re
-operators = { '=': 'Assig_Op','+': 'Add_Op', '<=': 'GTOE_Op'}
+operators = { '=': '=','+': '+', '<=': '<=', '==': '==', '<<': '<<', '>>': '>>'}
 optr_keys = operators.keys()
 
-comments = {r'//' : 'Single Line Comment'}
-comment_keys = comments.keys()
-
-datatype = {'int': 'Integer', 'char': 'Character'}
-datatype_keys = datatype.keys()
-
-keyword = {'return' : 'keyword that returns a value from a block'}
+keyword = {'return' : 'return', 'using': 'using', 'namespace': 'namespace', 'std': 'std', 'for': 'for', 'while': 'while', 'cout': 'cout', 'int': 'int', 'char': 'char'}
 keyword_keys = keyword.keys()
 
-delimiter = {';':'terminator symbol semicolon (;)'}
+delimiter = {';':';'}
 delimiter_keys = delimiter.keys()
 
-blocks = {'{' : 'Blocked Statement Body Open', '}':'Blocked Statement Body Closed'}
-block_keys = blocks.keys()
-
-token = ['keyword', 'identifier', 'symbol', 'operator', '']
-
-non_identifiers = ['_','-','+','/','*','`','~','!','@','#','$','%','^','&','*','(',')','=','|','"',':',';','{'
-,'}','[',']','<','>','?','/']
-
-numerals = ['0','1','2','3','4','5','6','7','8','9','10']
+non_identifiers = {'_','`','~','!','@','#','$','%','^','&','*','(',')','|','"',':',';','{'
+,'}','[',']','?'}
 
 # Flags
 dataFlag = False
 output = open("output_tokens_887111342.txt","w")
-output.write("tokens               lexemes\n_____________________________\n")
+output.write("token               lexemes\n_____________________________\n")
 
 def lexer(f, program):   
     f = open("input_sourcetext-1.txt", "r")
@@ -38,47 +24,50 @@ def lexer(f, program):
         count = count+1
         print ("Line #",count,"\n",line)
         
-        
         lexemes = line.split()
-        print ("Lexemes are", lexemes)
-        print ("Line #", count, 'properties \n')
-        for token in lexemes:
-            if '\r' in token:
-                position = token.find('\r')
-                token=token[:position]
-            if token in block_keys:
-                print(blocks[token])
-                # output.write("symbol               " + blocks[token])
-            if token in optr_keys:
-                print("Operator is: ", operators[token])
-                output.write("Operator               " + operators[token] + '\n')
-            if '()' in token:
-                print("Function named", token)
-            if token in comment_keys:
-                print ("Comment Type: ", comments[token])
+        # list_cycle = itertools.cycle(lexemes)
+        # next(list_cycle)
+        print ("Lexemes are: ", lexemes, "\n" )
+        print ("Line #", count, 'properties: ')
+        for index, lexeme in enumerate(lexemes):
+            if '\n' in lexeme:
+                position = lexeme.find('\n')
+                lexeme=lexeme[:position]
+            if lexeme in optr_keys:
+                print("Operator is: ", operators[lexeme])
+                output.write("Operator                " + operators[lexeme] + '\n')
+            if lexeme in keyword_keys:
+                print("Keyword is: ", keyword[lexeme])
+                output.write("Keyword               " + keyword[lexeme] + '\n')
+            if lexeme[-1] == ';':
+                if(lexeme.split(';')[0].isnumeric()):
+                    print("Number is: ", int(lexeme.split(';')[0]))
+                    output.write("Number                  " + (lexeme.split(';')[0]) + '\n')
+                    print("Symbol is: ", lexeme[-1])
+                    output.write("Symbol                  " + lexeme[-1] + '\n')
+            if lexeme in non_identifiers:
+                print("Symbol is: ", lexeme)
+                output.write("Symbol                  " + lexeme + '\n')
 
-            if dataFlag == True and (token not in non_identifiers) and ('()' not in token):
-                print("Identifier: ", token)
-                output.write("Identifier               " + token + '\n')
-            if token in datatype_keys:
-                print("type is: ", datatype[token])
-                output.write("Keyword               " + datatype[token] + '\n')
-                dataFlag = True
+            if len(lexeme) == 1:
+                if lexeme.isalpha():
+                    print("Identifier is: ", lexeme)
+                    output.write("Identifier              " + lexeme + '\n')
+                    
+            if lexeme[0] == '"':
+                string = ''
+                while(lexeme[-2] != '"'):
+                    print(lexeme)
+                    print(lexeme[index+1])
+                    string = string + lexeme
+                    string = string + ' ' + lexemes[index+1]
+                    lexeme = lexemes[index+1]
+                print("String is: ", string.split(";"))
+                output.write("String              " + string + '\n')
+                print("Symbol is: ", lexeme[-1])
+                output.write("Symbol                  " + lexeme[-1] + '\n')
 
-            if token in keyword_keys:
-                print(keyword[token])   
-
-            if token in delimiter:
-                print("Delimiter" , delimiter[token])
-            if '#' in token:
-                match = f.search(r'#\w+', token)
-                print("Header", match.group())
-            if token in numerals:
-                print(token,type(int(token)))
-
-        dataFlag = False   
         f.close()
-
 
 def main():
     f = open("input_sourcetext-1.txt", "r")
